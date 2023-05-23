113c66c868e3fc433b51ec6ffffe71a3ae406265
Ahmed - Drago <119659334+qithoniq@users.noreply.github.com>
2023-05-23 05:54:27 +0300

    Update secret.py

--- drago/assistant/secret.py
+++ drago/assistant/secret.py
@@ -4,25 +4,25 @@ import re
 
 from telethon.events import CallbackQuery
 
-from jepthon import jepiq
+from drago import dragoiq
 
 
 @jepiq.tgbot.on(CallbackQuery(data=re.compile(b"rzan_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./jepthon/secrets.txt"):
-        jsondata = json.load(open("./jepthon/secrets.txt"))
+    if os.path.exists("./drago/secrets.txt"):
+        jsondata = json.load(open("./drago/secrets.txt"))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
-            ids = [userid, jepiq.uid]
+            ids = [userid, dragoiq.uid]
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
             else:
                 reply_pop_up_alert = "⌯︙عـذرا هذه الهـمسة ليست مخصصة لـك"
         except KeyError:
-            reply_pop_up_alert = "⌯︙عـذرا هذه الهمسة لم تعد موجوده في سيـرفرات جـيبثون"
+            reply_pop_up_alert = "⌯︙عـذرا هذه الهمسة لم تعد موجوده في سيـرفرات دراكو"
     else:
         reply_pop_up_alert = "⌯︙عـذرا هذه الهمسة لم تعد موجوده  "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)

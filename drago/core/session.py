d8f11ffb9e893900e1aa22adf949b687f26745bf
MaTrix <119659334+qithoniq@users.noreply.github.com>
2023-08-01 23:59:19 +0300

    Update session.py

--- drago/core/session.py
+++ drago/core/session.py
@@ -1,24 +1,23 @@
 import sys
-
+from drago.core.logger import logging
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
-
+from telethon.sessions import StringSession
+from telethon.errors import AccessTokenExpiredError, AccessTokenInvalidError
 from ..Config import Config
-from .bothseesion import bothseesion
 from .client import DragoClient
-from .logger import logging
+LOGS = logging.getLogger(" ")
 
-LOGS = logging.getLogger("drago")
-__version__ = "1.0.0"
+__version__ = "2.10.6"
 
 loop = None
 
 if Config.STRING_SESSION:
-    session = bothseesion(Config.STRING_SESSION, LOGS)
+    session = StringSession(str(Config.STRING_SESSION))
 else:
-    session = "Sessionszbot"
+    session = "Drago_dr"
 
 try:
-    sbb_b = DragoClient(
+    dragoiq = DragoClient(
         session=session,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
@@ -29,18 +28,21 @@ try:
         connection_retries=None,
     )
 except Exception as e:
-    print(
-        f"STRING_SESSION WRONG PLZ MAKE A NEW SESSION - {e}\n كود سيشن تيليثون الذي وضعته غير صالح"
-    )
+    print(f"[STRING SESSION] - {str(e)}")
     sys.exit()
 
-sbb_b.tgbot = tgbot = DragoClient(
-    session="Sessionszbot",
-    api_id=Config.APP_ID,
-    api_hash=Config.API_HASH,
-    loop=loop,
-    app_version=__version__,
-    connection=ConnectionTcpAbridged,
-    auto_reconnect=True,
-    connection_retries=None,
-).start(bot_token=Config.TG_BOT_TOKEN)
+try:
+    dragoiq.tgbot = tgbot = DragoClient(
+        session="Sessionszbot",
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    ).start(bot_token=Config.TG_BOT_TOKEN)
+except AccessTokenExpiredError:
+    LOGS.error("توكن البوت منتهي الصلاحية قم باستبداله ليعمل السورس")
+except AccessTokenInvalidError:
+    LOGS.error("توكن البوت غير صحيح قم باستبداله ليعمل السورس")

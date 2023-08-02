97fd680911cd552c06df6c73ba532be905e07a1c
MaTrix <119659334+qithoniq@users.noreply.github.com>
2023-08-03 01:46:50 +0300

    Update الفارات.py

--- drago/plugins/الفارات.py
+++ drago/plugins/الفارات.py
@@ -658,43 +658,6 @@ async def variable(event):
             await dra.edit("**⌁︙تم بنجاح تغيير زخرفة الصورة الوقتية\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
         heroku_var[variable] = zinfo
 
-@dragoiq.ar_cmd(pattern="ميوزك(?:\s|$)([\s\S]*)")
-async def variable(event):
-    if Config.HEROKU_API_KEY is None:
-        return await edit_delete(
-            event,
-            "اضبط Var المطلوب في Heroku على وظيفة هذا بشكل طبيعي `HEROKU_API_KEY` اذا كنت لاتعلم اين يوجد فقط اذهب الى حسابك في هيروكو ثم الى الاعدادات ستجده بالاسفل انسخه ودخله في الفار. ",
-        )
-    if Config.HEROKU_APP_NAME is not None:
-        app = Heroku.app(Config.HEROKU_APP_NAME)
-    else:
-        return await ed(
-            event,
-            "اضبط Var المطلوب في Heroku على وظيفة هذا بشكل طبيعي `HEROKU_APP_NAME` اسم التطبيق اذا كنت لاتعلم.",
-        )
-    input_str = event.pattern_match.group(1)
-    heroku_var = app.config()
-    dra = await edit_or_reply(event, "** جارِ تغير وضع الميوزك ✅ . . .**")
-    if input_str == "تفعيل":
-        variable = "VCMODE"
-        zinfo = "True"
-        await asyncio.sleep(1.5)
-        if variable in heroku_var:
-            await dra.edit("**⌁︙تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
-        else:
-            await dra.edit("**⌁︙تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
-        heroku_var[variable] = zinfo
-    elif input_str == "تعطيل":
-        variable = "VCMODE"
-        zinfo = "False"
-        await asyncio.sleep(1.5)
-        if variable in heroku_var:
-            await dra.edit("**⌁︙تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
-        else:
-            await dra.edit("**⌁︙تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
-        heroku_var[variable] = zinfo
-
-
 @dragoiq.ar_cmd(pattern="استخدامي$")
 async def dyno_usage(dyno):
     if (HEROKU_APP_NAME is None) or (HEROKU_API_KEY is None):
